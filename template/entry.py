c1b9b8a0d230122b64f1fe9ad35f43ec17cce188
JasperBRiedel <jasperandbuddy@gmail.com>
2020-07-20 20:10:57 +1000

    added default text tileset

--- template/entry.py
+++ template/entry.py
@@ -11,26 +11,28 @@ def __config__():
 
         # Determines the initial viewport size and scale
         "viewport_scale": 2,
-        "viewport_width": 10,
-        "viewport_height": 10,
+        "viewport_width": 32,
+        "viewport_height": 32,
 
         # tileset path
         "tileset_path": "tiles.png",
         # Number of tiles along the horizontal axis
-        "tileset_width": 2,
+        "tileset_width": 40,
         # Number of tiles along the vertical axis
-        "tileset_height": 4,
+        "tileset_height": 3,
 
         # Names of each tile in order from left to right, top to bottom.
         "tile_names": [
-            "square",
-            "circle_1",
-            "circle_2",
-            "circle_3",
-            "tree",
-            "flowers",
-            "read_the_docs",
-            "pyrite"
+            "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*",
+            "+", ",", "-", ".", "/", "0", "1", "2", "3", "4",
+            "5", "6", "7", "8", "9", ":", ";", "<", "=", ">",
+            "?", "@", "A", "B", "C", "D", "E", "F", "G", "H",
+            "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+            "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\",
+            "]", "^", "_", "`", "a", "b", "c", "d", "e", "f",
+            "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+            "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+            "{", "|", "}", "~",
         ]
     }
 
@@ -38,10 +40,10 @@ def __event__(event_type, event_data):
     game_data = pyrite.game_data()
 
     if event_type == "LOAD":
-        game_data["draw_trees_timer"] = 0.0;
+        game_data["draw_bg_timer"] = 0.0;
 
-        game_data["viewport_width"] = 10
-        game_data["viewport_height"] = 10
+        game_data["viewport_width"] = 32
+        game_data["viewport_height"] = 32
         game_data["viewport_scale"] = 2
         pass
 
@@ -70,41 +72,31 @@ def __event__(event_type, event_data):
             )
 
     if event_type == "STEP":
-        game_data["draw_trees_timer"] -= event_data["delta_time"]
+        # colourful background
+        game_data["draw_bg_timer"] -= event_data["delta_time"]
+        if game_data["draw_bg_timer"] <= 0.0:
+            game_data["draw_bg_timer"] += 0.5
+            draw_random_bg()
 
-        if game_data["draw_trees_timer"] <= 0.0:
-            game_data["draw_trees_timer"] += 0.5
-
-            for x in range(10):
-                for y in range(10):
-                    position = (x, y)
-                    if should_plant_tree():
-                        pyrite.set_tile(
-                            position,
-                            "tree",
-                            pick_plant_color(),
-                            (False, False),
-                            "flowers",
-                            pick_plant_color(),
-                            (False, False),
-                        );
-                    else:
-                        pyrite.set_tile(
-                            position,
-                            "flowers",
-                            pick_plant_color(),
-                            (False, False),
-                        );
-
-        mouse_left = pyrite.button_down("MOUSE_LEFT")
-        mouse_right = pyrite.button_down("MOUSE_RIGHT")
-        pyrite.set_tile((4, 1), "pyrite", (255, 255, 255), (mouse_left, mouse_right))
-        pyrite.set_tile((5, 1), "read_the_docs", (255, 255, 255), (False, False))
+        # instructions
+        draw_rect(4, 4, 13, 3, "fill", (0, 0, 0))
+        draw_string(5, 5, "Hello, World!")
 
 
-def pick_plant_color():
-    return random.choice([(1,145,135), (146,196,86), (199,228,128), (221,193,85)])
 
-def should_plant_tree():
-    return random.choice([True, False])
+def draw_string(x, y, s):
+    for i, c in enumerate(s):
+        pyrite.set_tile((x + i, y), c, (255, 255, 255), (False, False))
+
+def draw_rect(x, y, width, height, tile, color):
+    for cx in range(width):
+        for cy in range(height):
+            pyrite.set_tile((cx + x, cy + y), tile, color, (False, False)) 
+
+def draw_random_bg():
+    for x in range(32):
+        for y in range(32):
+            tile = random.choice(list("!@#$%^&*"))
+            color = random.choice([(1,145,135), (146,196,86), (199,228,128), (221,193,85)])
+            pyrite.set_tile((x, y), tile, color, (False, False))
 
